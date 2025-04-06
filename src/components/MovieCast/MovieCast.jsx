72bd09b61c80fb742cd0f244e72ce9fd38b939d6
Evgen Fedorchenko <135177643+NayxieX@users.noreply.github.com>
2025-04-06 15:48:32 +0300

    Upd  Styles and fonts

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -8,14 +8,6 @@ export default function MovieCast() {
   const location = useLocation();
   const { movieId } = useParams();
 
-  // const url = location.pathname;
-  // function extractNumbersFromUrl(url) {
-  //   return Array.from(url)
-  //     .filter((char) => !isNaN(char) && char !== " ")
-  //     .join("")
-  //     .toString();
-  // }
-  // const id = extractNumbersFromUrl(url);
   const defaultImg =
     "<https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg>";
   useEffect(() => {
